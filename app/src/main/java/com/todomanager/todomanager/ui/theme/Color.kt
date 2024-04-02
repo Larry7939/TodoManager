2c79d1ba12ee764234c96c910a37189cd2bb6f72
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2024-04-03 00:35:55 +0900

    [add] 컬러 리소스 추가 및 Theme 설정

--- app/src/main/java/com/todomanager/todomanager/ui/theme/Color.kt
+++ app/src/main/java/com/todomanager/todomanager/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val G1 = Color(0xFF000000)
+val G2 = Color(0xFF5A5A5A)
+val G3 = Color(0xFF8B8B8B)
+val G4 = Color(0xFFA3A3A3)
+val G5 = Color(0xFFD9D9D9)
+val G6 = Color(0xFFE7E7E7)
+
+val B1 = Color(0xFF00B0F0)
