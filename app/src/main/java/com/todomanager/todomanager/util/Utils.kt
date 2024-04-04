8169a2333c030c33d7388165ea13acb0f8e9a0d7
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2024-04-05 05:09:22 +0900

    [feat] toast util 함수 구현

--- app/src/main/java/com/todomanager/todomanager/util/Utils.kt
+++ app/src/main/java/com/todomanager/todomanager/util/Utils.kt
@@ -2,10 +2,15 @@ package com.todomanager.todomanager.util
 
 import android.Manifest
 import android.content.Context
+import android.widget.Toast
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.normal.TedPermission
 
 object Utils {
+    fun Context.showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
     fun requestPermission(context: Context, permission: String, logic : () -> Unit) {
         TedPermission.create()
             .setPermissionListener(object  : PermissionListener {
