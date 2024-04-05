e883233f4c4892ea234523d63b66442413bdfba7
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2024-04-05 23:37:49 +0900

    [feat] DI 구현 - LocalDataModule

---
+++ app/src/main/java/com/todomanager/todomanager/di/LocalDataModule.kt
@@ -0,0 +1,37 @@
+package com.todomanager.todomanager.di
+
+import android.content.Context
+import com.todomanager.todomanager.datasource.LocalDataSource
+import com.todomanager.todomanager.repository.local.LocalRepository
+import com.todomanager.todomanager.repository.local.LocalRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalDataModule {
+
+    @Singleton
+    @Provides
+    fun provideLocalDataSource(@ApplicationContext context: Context, json: Json): LocalDataSource = LocalDataSource.getInstance(context, json)
+
+    @Singleton
+    @Provides
+    fun bindLocalRepository(localRepositoryImpl: LocalRepositoryImpl): LocalRepository = localRepositoryImpl
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json {
+        isLenient = true
+        prettyPrint = true
+        ignoreUnknownKeys = true
+    }
+}
