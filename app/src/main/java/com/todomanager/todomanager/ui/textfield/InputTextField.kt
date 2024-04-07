c0a223538f93c5249d00b2be4e2783d363060454
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2024-04-07 19:08:52 +0900

    [feat] TaskDateTextField 구현
    Task 추가/수정 뷰에서 사용되는 DateTextField 구현

--- app/src/main/java/com/todomanager/todomanager/ui/textfield/InputTextField.kt
+++ app/src/main/java/com/todomanager/todomanager/ui/textfield/InputTextField.kt
@@ -3,7 +3,9 @@ package com.todomanager.todomanager.ui.textfield
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,6 +15,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,12 +29,10 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.todomanager.todomanager.R
+import com.todomanager.todomanager.dto.TaskDate
 import com.todomanager.todomanager.ui.theme.B1
 import com.todomanager.todomanager.ui.theme.G5
 import com.todomanager.todomanager.ui.theme.TodoManagerTheme
@@ -42,6 +43,7 @@ class InputTextField {
     @Composable
     fun CustomOutlinedTextField(
         hint: String,
+        text: String = "",
         maxLines: Int = 1,
         maxLength: Int = 50,
         focusRequester: FocusRequester,
@@ -50,7 +52,9 @@ class InputTextField {
     ) {
         var textState by rememberSaveable { mutableStateOf("") }
         var isFocused by remember { mutableStateOf(false) }
-
+        LaunchedEffect(text) {
+            textState = text
+        }
 
         Box(
             modifier = Modifier
@@ -62,7 +66,7 @@ class InputTextField {
         ) {
             BasicTextField(modifier = Modifier
                 .focusRequester(focusRequester)
-                .padding(start = 30.dp)
+                .padding(start = 30.dp, end = 10.dp)
                 .onFocusChanged {
                     isFocused = it.isFocused
                 },
@@ -117,6 +121,48 @@ class InputTextField {
                 color = textColor,
                 style = Typography.bodyLarge
             )
+        }
+    }
+
+    @Composable
+    fun TaskDateTextField(hint: String, taskDate: TaskDate, onClick: () -> Unit) {
+        val isHintVisible = taskDate.isEmpty()
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(65.dp)
+                .padding(horizontal = 20.dp)
+                .border(width = 2.dp, B1, shape = RoundedCornerShape(30.dp))
+                .clickable(
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() }) { onClick() }
+        ) {
+            if (isHintVisible) {
+                Text(
+                    modifier = Modifier
+                        .padding(start = 30.dp)
+                        .align(Alignment.CenterStart),
+                    text = hint,
+                    color = G5,
+                    style = Typography.bodyLarge
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Center),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                Text(
+                    modifier = Modifier,
+                    text = taskDate.year,
+                    style = Typography.bodyLarge
+                )
+                Text(text = taskDate.monthDate)
+                Text(text = taskDate.hourMinute)
+            }
         }
     }
 
