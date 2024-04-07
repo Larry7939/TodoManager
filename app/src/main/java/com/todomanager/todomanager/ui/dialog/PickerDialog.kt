b0069604a1cc612fa322b0a627671c1e8042c8eb
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2024-04-07 19:07:55 +0900

    [feat] TimePicker 구현
    - Task 추가/수정 시 사용할 TimePicker 구현

--- app/src/main/java/com/todomanager/todomanager/ui/dialog/PickerDialog.kt
+++ app/src/main/java/com/todomanager/todomanager/ui/dialog/PickerDialog.kt
@@ -1,25 +1,24 @@
 package com.todomanager.todomanager.ui.dialog
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.wrapContentSize
+import android.app.TimePickerDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerColors
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import com.todomanager.todomanager.R
 import com.todomanager.todomanager.ui.theme.B1
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 
@@ -85,6 +84,40 @@ class PickerDialog {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun CustomTimePicker(
+        onTimeSelected: (String) -> Unit,
+        onDismiss: () -> Unit
+    ): TimePickerDialog {
+        val calendar = remember { Calendar.getInstance() }
+        val timeState = remember { mutableStateOf("") }
+        val context = LocalContext.current
+        val timePickerDialog = TimePickerDialog(
+            context,
+            { view, hourOfDay, minute ->
+                val adjustedHour = if (hourOfDay > 12) {
+                    hourOfDay - 12
+                } else {
+                    hourOfDay
+                }
+                val adjustedMinute = String.format("%02d", minute)
+                val amPm = if (hourOfDay < 12) {
+                    "AM"
+                } else {
+                    "PM"
+                }
+
+                timeState.value = "${adjustedHour}:${adjustedMinute} $amPm"
+                onTimeSelected(timeState.value)
+            },
+            calendar[Calendar.HOUR_OF_DAY],
+            calendar[Calendar.MINUTE],
+            false
+        )
+        timePickerDialog.setOnCancelListener { onDismiss() }
+        return timePickerDialog
+    }
 
     private fun convertMillisToDate(pattern: String, millis: Long): String {
         val formatter = SimpleDateFormat(pattern, Locale.KOREA)
